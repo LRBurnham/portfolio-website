0f2ae589755afc6b90590d8f50568f05de93eb01
LR Burnham <lilyroocodes@gmail.com>
2022-08-28 04:12:20 -0400

    Setup for wtforms implementation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
 import os
 
 # TODO: create images, replace placeholders
@@ -9,9 +12,17 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send Message')
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = ContactForm()
+    return render_template('index.html', form=form)
 
 
 if __name__ == "__main__":
